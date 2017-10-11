974f176c20d3f0b1400a6dc9b5f80b784ea24b28
Annie  Nguyen <annienguyen@Annies-MacBook-Air.local>
2017-10-10 21:15:43 -0700

    auth-afternoon saved

--- server/strategy.js
+++ server/strategy.js
@@ -5,7 +5,7 @@ const { domain, clientID, clientSecret } = config;
 module.exports = new Auth0Strategy({
    domain:       'annienguyen.auth0.com',
    clientID:     '8EsamKaKwkTAwVvk1KN90xFNE03yDRkS',
-   clientSecret: '9MCRN4a6tFP8AraU7yRQSQ2orIGTc4Fz1lxvdwN2RQzSU27D8vUDwWPpm7nZvIvz',
+   clientSecret: '',
    callbackURL:  'http://localhost:3030/auth/callback'
   },
   function(accessToken, refreshToken, extraParams, profile, done) {
